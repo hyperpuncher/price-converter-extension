143342fb50a39591fa746c19df19a707b63fa1b5
hyperpuncher <imolohovskiy@gmail.com>
2023-05-09 10:05:04 +0300

    refactor and bugfix

--- kufar.js
+++ kufar.js
@@ -1,40 +1,53 @@
-const tags = ["[class*=styles_price--main__]", "[class*=styles_price__]", "[class*=styles_main]", "[class*=styles_discountPrice]", ".account_ads__price"];
+const selectors = [
+  "[class*=styles_price--main__]",
+  "[class*=styles_price__]",
+  "[class*=styles_main]",
+  "[class*=styles_discountPrice]",
+  ".account_ads__price",
+];
 
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+let rate;
 
-var observer = new MutationObserver(function (mutations, observer) {
-  function conversion(rate) {
-    for (tag of tags) {
-      if (document.querySelector(tag)) {
-        (function () {
-          let elts = document.querySelectorAll(tag);
-          for (elt of elts) {
-            let price = convertor(elt);
-            if (!isNaN(price)) {
-              elt.innerHTML += '<span> ' + price.toFixed(2) + ' $' + '</span>';
-            }
-          }
-        })(tag);
+(async () => {
+  const response = await fetch("https://www.nbrb.by/api/exrates/rates/431");
+  const data = await response.json();
+  rate = data["Cur_OfficialRate"];
+})();
+
+function addConversion() {
+  for (let selector of selectors) {
+    let elements = document.querySelectorAll(selector);
+
+    if (elements) {
+      for (let element of elements) {
+        let price = convertToDollars(element);
+
+        if (!isNaN(price)) {
+          element.innerHTML += "<span> " + price.toFixed(2) + " $" + "</span>";
+        }
       }
-    }
-    // Cleans up string to number and converts to dollars
-    function convertor(elt) {
-      if ((!elt.innerText.includes('%')) && (!elt.innerText.includes('$')) && (elt.innerText.includes('р.'))) {
-        let price = parseFloat(elt.innerText.replace(/[^0-9.]/g, ''));
-        let conversion = price / rate;
-        return conversion;
-      }
-      return NaN;
     }
   }
+}
 
-  fetch('https://www.nbrb.by/api/exrates/rates/431')
-    .then(response => response.json())
-    .then(data => conversion(data["Cur_OfficialRate"]));
+function convertToDollars(element) {
+  let text = element.innerText;
 
+  if (!text.includes("%") && !text.includes("$") && text.includes("р.")) {
+    let price = parseFloat(text.replace(/[^0-9.]/g, ""));
+    let conversion = price / rate;
+    return conversion;
+  }
+  return NaN;
+}
+
+const observer = new window.MutationObserver(() => {
+  if (rate) {
+    addConversion();
+  }
 });
 
 observer.observe(document, {
   subtree: true,
-  attributes: true
+  attributes: true,
 });
--- onliner.js
+++ onliner.js
@@ -1,67 +1,99 @@
-const tags = [".schema-product__price-value", ".product-summary__price-value", ".ppricevalue", ".js-description-price-link", ".js-short-price-link", ".product-recommended__price", ".price-primary", ".offers-description__price", ".offers-list__description_nodecor", ".offers-list__price_secondary", ".offers-list__price_primary", ".cart-form__description_condensed-another", ".cart-form__description_font-weight_semibold", ".cart-form__description_extended", ".schema-product__button"];
+const selectors = [
+  ".schema-product__price-value",
+  ".product-summary__price-value",
+  ".ppricevalue",
+  ".js-description-price-link",
+  ".js-short-price-link",
+  ".product-recommended__price",
+  ".price-primary",
+  ".offers-description__price",
+  ".offers-list__description_nodecor",
+  ".offers-list__price_secondary",
+  ".offers-list__price_primary",
+  ".cart-form__description_ellipsis",
+  ".cart-form__description_extended",
+  ".schema-product__button",
+];
 
-var formatter = new Intl.NumberFormat('ru-RU', {
-  style: 'currency',
-  currency: 'USD'
+let rate;
+
+(async () => {
+  const response = await fetch("https://www.nbrb.by/api/exrates/rates/431");
+  const data = await response.json();
+  rate = data["Cur_OfficialRate"];
+})();
+
+const formatter = new Intl.NumberFormat("ru-RU", {
+  style: "currency",
+  currency: "USD",
 });
 
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+function addConversion() {
+  for (let selector of selectors) {
+    let elements = document.querySelectorAll(selector);
 
-var observer = new MutationObserver(function (mutations, observer) {
-  function conversion(rate) {
-    for (tag of tags) {
-      if (document.querySelector(tag)) {
-        (function () {
-          let elts = document.querySelectorAll(tag);
-          for (elt of elts) {
-            let price = convertor(elt);
-            if (price) {
-              if (tag != ".cart-form__description_extended") {
-                elt.innerHTML += '<br><span style="text-shadow: 0px 0px 0.8px">' + price + '</span>';
-              } else {
-                elt.innerText += ' ' + price;
-              }
-            }
-          }
-        })(tag);
-      }
-    }
-    // Cleans up string to number and converts to dollars
-    function convertor(elt) {
-      if ((!elt.innerText.includes('$')) && ((elt.innerText.includes('р.')) || (elt.innerText.includes('p.')))) {
-        if ((elt.innerText.includes('–')) && (!elt.innerText.includes('%'))) {
-          const range = elt.innerText.split('–');
-          const rangeDollar = [];
-          for (elt of range) {
-            let price = parseFloat(elt.replace(/[^0-9,]/g, '').replace(',', '.'));
-            let conversion = price / rate;
-            rangeDollar.push(formatter.format(conversion));
-          }
-          let dollar = rangeDollar[0] + ' – ' + rangeDollar[1];
-          dollar = dollar.replace('$', '');
-          return dollar;
-        } else if (elt.innerText.includes(':')) {
-          let price = parseFloat(elt.innerText.split(':').pop().replace(/[^0-9,]/g, '').replace(',', '.'));
-          let conversion = price / rate;
-          let dollar = formatter.format(conversion);
-          return dollar;
+    if (elements) {
+      for (let element of elements) {
+        let price = convertToDollars(element);
+
+        if (price) {
+          element.innerHTML +=
+            '<br><span style="text-shadow: 0px 0px 0.8px">' + price + "</span>";
         }
-        let price = parseFloat(elt.innerText.replace(/[^0-9,]/g, '').replace(',', '.'));
-        let conversion = price / rate;
-        let dollar = formatter.format(conversion);
-        return dollar;
       }
-      return null;
     }
   }
+}
 
-  fetch('https://www.nbrb.by/api/exrates/rates/431')
-    .then(response => response.json())
-    .then(data => conversion(data["Cur_OfficialRate"]));
+function convertToDollars(element) {
+  let text = element.innerText;
 
+  if (!text.includes("$") && text.includes("р.")) {
+    // Handle price ranges
+    if (text.includes("–") && !text.includes("%")) {
+      const range = text.split("–");
+      const rangeDollar = [];
+
+      for (element of range) {
+        let price = parseFloat(
+          element.replace(/[^0-9,]/g, "").replace(",", ".")
+        );
+        let conversion = price / rate;
+        rangeDollar.push(formatter.format(conversion));
+      }
+
+      let dollar = rangeDollar[0] + " – " + rangeDollar[1];
+      dollar = dollar.replace("$", "");
+      return dollar;
+      // Handle prices with a colon separator
+    } else if (text.includes(":")) {
+      let price = parseFloat(
+        text
+          .split(":")
+          .pop()
+          .replace(/[^0-9,]/g, "")
+          .replace(",", ".")
+      );
+      let conversion = price / rate;
+      let dollar = formatter.format(conversion);
+      return dollar;
+    }
+    // Handle regular prices
+    let price = parseFloat(text.replace(/[^0-9,]/g, "").replace(",", "."));
+    let conversion = price / rate;
+    let dollar = formatter.format(conversion);
+    return dollar;
+  }
+  return null;
+}
+
+const observer = new window.MutationObserver(() => {
+  if (rate) {
+    addConversion();
+  }
 });
 
 observer.observe(document, {
   subtree: true,
-  attributes: true
+  attributes: true,
 });
